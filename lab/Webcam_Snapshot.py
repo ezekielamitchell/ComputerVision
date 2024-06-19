020aa26deb7b9c01957ad96726a42e5e77b95456
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-06-18 22:24:21 -0700

    Webcam Snapshot

---
+++ lab/Webcam_Snapshot.py
@@ -0,0 +1,13 @@
+##### SETUP #####
+import cv2 as cv
+vid = cv.VideoCapture(0)
+##### BUILD #####
+while (True):
+    ret, frame = vid.read() # read frame
+    cv.imshow('frame', frame) # display frame
+
+    if cv.waitKey(1) & 0xFF == 'q':
+        break ## break once esc key is pressed
+
+vid.release() # release cap object
+cv.destroyAllWindows()
