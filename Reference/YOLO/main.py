f589da2d6e9ebd40fc502f7478f7040dc45636c2
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-09-19 18:51:07 -0700

    class identify

--- Reference/YOLO/main.py
+++ Reference/YOLO/main.py
@@ -1,33 +1,32 @@
+import subprocess
 import cv2
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt') # load YOLOv8 model
+subprocess.run(['python', './classes.py'])
 
-class_names = model.names # model classes
+# load YOLOv8 model
+model = YOLO('yolov8n.pt')
+# load video
+image_path ='./data/image0.jpg'
+video_path = './data/video0.mp4'
+cap = cv2.VideoCapture(video_path)
 
-dog_class_id = class_names.index('dog') if 'dog' in class_names else None
+# read video frame
+while True:
+    ret, frame = cap.read()
 
-if dog_class_id is not None:
-    image_path ='./data/image0.jpg' 
-    video_path = './data/video0.mp4'
-    cap = cv2.VideoCapture(video_path) # load video
+    if ret:
+        # apply object detection method & track detected objects
+        results = model.predict(source=frame, save=False, classes=[16]) # classes=[16] for dog
 
-    while True: # read video frame
-        ret, frame = cap.read()
+        frame_ = results[0].plot() # plot results
 
-        if ret:
-            # apply object detection method & track detected objects
-            results = model.track(frame, persist=True)
+        cv2.imshow('Find Dog', frame_) # visualize
 
-            for result in results: # filter results to only include 'dog' detections
-                result.boxes = result.boxes.cpu() # move boxes to CPU for filtering
-                result.boxes = result.boxes[result.boxes.cls==dog_class_id]
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    else:
+        break
 
-            frame_ = results[0].plot()
-
-            cv2.imshow('Find Dog', frame_) # visualize
-
-            if cv2.waitKey(10) & 0xFF == ord('q'):
-                break
-else:
-    print("error locating 'dog' class")
+cap.release()
+cv2.destroyAllWindows()
