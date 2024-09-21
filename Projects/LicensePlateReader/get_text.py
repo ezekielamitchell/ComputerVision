b77011d81084fd768435be67f4a75aee807b6efc
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-09-21 12:58:05 -0700

    basic functions pass

--- Projects/LicensePlateReader/get_text.py
+++ Projects/LicensePlateReader/get_text.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH", None)
+image_path = './data/located_plates/cropped_image.png'
 
 def get_text(image_path):
     
@@ -27,4 +28,8 @@ def get_text(image_path):
             cv2.putText(image, text, top_left, cv2.FONT_HERSHEY_COMPLEX, 0.65, (0,0,255), 1)
 
     return message
+
+message = get_text(image_path)
+with open('./data/located_plates/license_plates.txt', 'w') as file:
+    file.write(message + '\n')
 
