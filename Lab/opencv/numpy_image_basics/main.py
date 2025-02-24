840c6b17f229696e83d58176442d031049b4f2bd
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-02-23 18:30:19 -0800

    numpy image basics

--- Lab/opencv/numpy_image_basics/main.py
+++
@@ -1,1 +0,0 @@
-print("Hello, Robotics world 🤖!")
---
+++ Lab/opencv/python/numpy_image_basics/main.py
@@ -0,0 +1,19 @@
+import cv2
+
+# Load an image
+data_set = '/home/ezekiel/Developer/ComputerVision/Lab/opencv/data/images/'
+image = cv2.imread(data_set + 'image01.jpg')
+
+# Check if image was loaded successfully
+if image is None:
+    print('Error: image not found')
+else:
+    print('Image loaded successfully')
+
+    # Display the image
+    cv2.imshow('Image', image)
+
+    # Wait for a key press to close the window and exit
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
